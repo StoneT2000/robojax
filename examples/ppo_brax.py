92905c1e103a19d64cc8a5485627dfb5ca7da14a
stone_tao <stonezt2019@gmail.com>
2022-08-08 19:39:57 -0700

    brax runs

--- examples/ppo_brax.py
+++ examples/ppo_brax.py
@@ -17,28 +17,26 @@ from robojax.models.ac.core import ActorCritic
 from robojax.models.mlp import MLP
 
 environment = "ant"
-env = envs.create(env_name=environment)
+env = envs.create(env_name=environment, auto_reset=False, episode_length=1000)
 state = env.reset(rng=jax.random.PRNGKey(0))
 
-num_envs = 4
+num_envs = 32
 def env_step(rng_key, state, action):
-    print("ACTION", action)
-    return env.step(state, action)
+    state = env.step(state, action * 0)
+    return state.obs, state, state.reward, state.done != 0.0, state.info
 def env_reset(rng_key):
     state = env.reset(rng_key)
     return state.obs, state
-# env.step = env_step
-# env.reset = env_reset
-algo = PPO(env_step, env_reset, jax_env=True)
+algo = PPO(env_step=env_step, env_reset=env_reset, jax_env=True)
 
 act_dims = env.action_size
-actor = MLP([64, 64, act_dims], output_activation=None)
-critic = MLP([64, 64, 1], output_activation=None)
+actor = MLP([256, 256, act_dims], output_activation=None)
+critic = MLP([256, 256, 1], output_activation=None)
 ac = ActorCritic(
     jax.random.PRNGKey(1),
     actor=actor,
     critic=critic,
-    explorer=explore.Categorical(),
+    explorer=explore.Gaussian(act_dims=act_dims),
     sample_obs=env.reset(jax.random.PRNGKey(0)).obs,
     act_dims=act_dims,
     actor_optim=optax.adam(learning_rate=1e-4),
@@ -46,10 +44,10 @@ ac = ActorCritic(
 )
 algo.train(
     rng_key=jax.random.PRNGKey(0),
-    steps_per_epoch=1000,
+    steps_per_epoch=2048*4,
     update_iters=80,
     num_envs=num_envs,
-    epochs=40,
+    epochs=4000,
     ac=ac,
-    batch_size=512,
+    batch_size=1024,
 )
