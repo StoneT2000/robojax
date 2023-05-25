00438cc02af3e3f3b7ac66579e7c02df8aee1d35
StoneT2000 <stonezt2019@gmail.com>
2023-05-25 14:28:13 -0700

    Update _mani_skill2.py

--- robojax/utils/make_env/_mani_skill2.py
+++ robojax/utils/make_env/_mani_skill2.py
@@ -17,7 +17,7 @@ def is_mani_skill2_env(env_id: str):
         return False
     from mani_skill2.utils.registration import REGISTERED_ENVS
 
-    return env_id in REGISTERED_ENVS or env_id in ["PickCube-v1", "PegInsertion-v1"]
+    return env_id in REGISTERED_ENVS or env_id in ["PickCube-v1", "PegInsertionSide-v1"]
 
 
 def env_factory(env_id: str, idx: int, seed: int, env_kwargs=dict(), record_video_path: str = None, wrappers=[]):
